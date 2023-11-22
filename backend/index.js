3a60e0af4f6a25bd6a6517e67a224367f5debf88
AlekseySpirin <spirin.mezhmun@yandex.ru>
2023-11-22 18:24:52 +0300

    fix: models

--- backend/index.js
+++ backend/index.js
@@ -3,23 +3,29 @@ require('dotenv').config();
 const express = require('express');
 const sequelize = require('./db');
 const PORT = process.env.PORT;
-const models = require('./models/models')
+const models = require('./models/models');
 const app = express();
-const cors = require('cors')
+const cors = require('cors');
 const router = require('./routes/index');
+const errorHandler = require('./middleware/ErrorHandlingMiddleware');
+const fileUpload = require('express-fileupload')
+const path = require('path')
 
-app.use(cors())
-app.use(express.json())
+app.use(cors());
+app.use(express.json());
+app.use(express.static(path.resolve(__dirname, 'static')))
+app.use(fileUpload({}))
+app.use('/api', router);
 
-app.use('/api', router)
+app.use(errorHandler);
 const start = async () => {
 	try {
-		await sequelize.authenticate()
-		await sequelize.sync()
+		await sequelize.authenticate();
+		await sequelize.sync();
 		app.listen(PORT, () => console.log(`Сервер запущен на порту: ${PORT}`));
 	} catch (e) {
 		console.log(e);
 	}
 };
 
-start()
+start();
